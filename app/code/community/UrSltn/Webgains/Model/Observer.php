649d4392376c9f8b0c65553611e9852c40685055
Shaughn <shaughn@silvertree.holdings>
2017-02-09 16:26:40 +0200

    Added check to Updated Convert/Parser/Product actions_xml to the expected webgains model.

--- app/code/community/UrSltn/Webgains/Model/Observer.php
+++ app/code/community/UrSltn/Webgains/Model/Observer.php
@@ -55,6 +55,19 @@ class UrSltn_Webgains_Model_Observer
         $this->helper()->debug(sprintf('%s - END', __METHOD__));
     }
 
+    public function checkConvertParserProduct(Varien_Event_Observer $observer)
+    {
+        $object = $observer->getEvent()->getData('object');
+
+        if ($object instanceof Mage_Dataflow_Model_Profile) {
+            if ($object->getName() == 'Webgains Product Export') {
+                $object->setData('actions_xml', '
+<action type="catalog/convert_adapter_product" method="load">\r\n    <var name="store"><![CDATA[0]]></var>\r\n</action>\r\n\r\n<action type="ursltn_webgains/convert_parser_product" method="unparse">\r\n    <var name="store"><![CDATA[0]]></var>\r\n</action>\r\n\r\n<action type="ursltn_webgains/convert_mapper_column" method="map">\r\n</action>\r\n\r\n<action type="dataflow/convert_parser_csv" method="unparse">\r\n    <var name="delimiter"><![CDATA[,]]></var>\r\n    <var name="enclose"><![CDATA["]]></var>\r\n    <var name="fieldnames">true</var>\r\n</action>\r\n\r\n<action type="dataflow/convert_adapter_io" method="save">\r\n    <var name="type">file</var>\r\n    <var name="path">var/export</var>\r\n    <var name="filename"><![CDATA[webgains_product_export.csv]]></var>\r\n</action>\r\n\r\n
+                ');
+            }
+        }
+    }
+
     /**
      * Helper
      *
